b470188e7c5f65d5611b7bbe63eb176c4483df0d
José Ramón Suffo Peimbert <suffo.jose@uabc.edu.mx>
2025-05-03 18:06:14 -0700

    Creado tablero de Memorama y cambios en clase Main.

--- Main.java
+++ Main.java
@@ -1,2 +1,106 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Main {
+    public static void main(String[] args) {
+        JFrame ventana = new JFrame("Memorama");
+        ventana.setSize(500,500);
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ventana.setLocationRelativeTo(null);
+
+        JButton jugar = new JButton("Jugar");
+        JButton creditos = new JButton("Creditos");
+        JButton salir = new JButton("Salir");
+
+        jugar.setBounds(100,300,100,50);
+        creditos.setBounds(200,300,100,50);
+        salir.setBounds(300,300,100,50);
+
+        ventana.add(jugar);
+        ventana.add(creditos);
+        ventana.add(salir);
+        ventana.setLayout(null);
+        ventana.setVisible(true);
+
+        ImageIcon pic = new ImageIcon("C:\\Users\\RedBo\\OneDrive\\Escritorio\\POO\\Practica7\\FondoMain.png");
+        JLabel image = new JLabel(pic);
+        image.setBounds(0,0,pic.getIconWidth(),pic.getIconHeight());
+        ventana.add(image);
+
+        //Boton Jugar
+
+        jugar.addActionListener(new ActionListener() {
+           public void actionPerformed(ActionEvent e) {
+               int numJugadores = 0;
+               while (numJugadores < 2 || numJugadores > 4) {
+                   String numJugadoresStr = JOptionPane.showInputDialog(ventana,
+                           "Numero de jugadores",
+                           "Jugadores",
+                           JOptionPane.QUESTION_MESSAGE);
+                   numJugadores = Integer.parseInt(numJugadoresStr);
+                   Object[] botones = {"5", "10","25", "Todos"};
+                   int modalidadOpcion = JOptionPane.showOptionDialog(ventana,
+                           "Elige modalidad",
+                           "Modalidad",
+                           JOptionPane.DEFAULT_OPTION,
+                           JOptionPane.QUESTION_MESSAGE,
+                           null,
+                           botones,
+                           botones[0]);
+                   int puntuacionMaxima = 0;
+                   switch (modalidadOpcion) {
+                       case 0:
+                           puntuacionMaxima = 5;
+                           break;
+                       case 1:
+                           puntuacionMaxima = 10;
+                           break;
+                       case 2:
+                           puntuacionMaxima = 25;
+                           break;
+                   }
+                   Object[] botones2 = {"Bandera", "Numero", "Raza de Dragon Ball"};
+                   int figuraMemorama = JOptionPane.showOptionDialog(ventana,
+                           "Elige modalidad",
+                           "Modalidad",
+                           JOptionPane.DEFAULT_OPTION,
+                           JOptionPane.QUESTION_MESSAGE,
+                           null,
+                           botones2,
+                           botones2[0]);
+                   String figura = "";
+                   switch (figuraMemorama) {
+                       case 0:
+                           figura = "Bandera";
+                           break;
+                       case 1:
+                           figura = "Numero";
+                           break;
+                       case 2:
+                           figura = "Raza de Dragon Ball";
+                           break;
+                   }
+                   Memorama juego = new Memorama(numJugadores, puntuacionMaxima, figura);
+               }
+           }
+        });
+
+        // boton creditos
+        creditos.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(ventana, "Realizado por: \n" +
+                                "Francisco Javier Oceguera Gutierrez" +
+                                "\n José Ramón Suffo Peimbert",
+                        "Créditos", JOptionPane.DEFAULT_OPTION);
+            }
+        });
+        // boton salir
+        salir.addActionListener(new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+    }
 }
--- Memorama.java
+++ Memorama.java
@@ -1,2 +1,63 @@
-public class Memorama {
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class Memorama extends JFrame{
+    private JTextArea puntosJugadores, cartasVolteadas;
+    private ArrayList<JButton> tarjetas;
+    private int jugadores, puntosMaximos;
+    private String figura;
+
+    public Memorama(int numJugadores, int puntuacionMaxima, String figura) {
+        super("Memorama");
+        tarjetas = new ArrayList<>();
+        this.jugadores = numJugadores;
+        this.puntosMaximos = puntuacionMaxima;
+        this.figura = figura;
+        setLocationRelativeTo(null);
+        inicializarComponentes();
+        configurarVentana();
+        this.setLocationRelativeTo(null);
+    }
+
+    public void inicializarComponentes(){
+        puntosJugadores = new JTextArea(20,20);
+        puntosJugadores.setEditable(false);
+        cartasVolteadas = new JTextArea(20,20);
+        cartasVolteadas.setEditable(false);
+        for(int i=0; i<16; i++){
+            JButton b = new JButton();
+            b.setSize(150,150);
+            b.setBackground(new Color(255, 152, 18));
+            tarjetas.add(b);
+        }
+        System.out.println(tarjetas.size());
+    }
+
+    public void configurarVentana(){
+        this.setLayout(new BorderLayout());
+        JPanel tablero = new JPanel();
+        tablero.setPreferredSize(new Dimension(660, 660));
+        tablero.setLayout(new GridLayout(4,4));
+        JPanel textos = new JPanel();
+        textos.setLayout(new GridLayout(1,2));
+        textos.setPreferredSize(new Dimension(660, 155));
+        for(int i=0; i<tarjetas.size(); i++){
+            JButton b = tarjetas.get(i);
+            tablero.add(b);
+        }
+        puntosJugadores.setFont(new Font("Monospaced", Font.PLAIN, 16));
+        puntosJugadores.setBackground(new Color(0, 255, 255));
+        puntosJugadores.setBorder(BorderFactory.createTitledBorder("Puntajes:"));
+        cartasVolteadas.setFont(new Font("Monospaced", Font.PLAIN, 16));
+        cartasVolteadas.setBackground(new Color(0, 255, 255));
+        cartasVolteadas.setBorder(BorderFactory.createTitledBorder("Cartas Volteadas:"));
+        textos.add(puntosJugadores);
+        textos.add(cartasVolteadas);
+        this.add(tablero, BorderLayout.NORTH);
+        this.add(textos, BorderLayout.SOUTH);
+        this.setSize(750, 850);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
 }
